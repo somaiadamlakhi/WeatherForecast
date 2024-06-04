5380b3a4aace1073088d91c2a4789c99ae9fdc3b
Sumaia Damlakhi <somaia.damlakhi@startappz.com>
2024-06-04 10:55:54 +0300

    create about screen

--- app/src/main/java/com/startappz/weatherforcast/screens/about/AboutScreen.kt
+++ app/src/main/java/com/startappz/weatherforcast/screens/about/AboutScreen.kt
@@ -1,11 +1,61 @@
 package com.startappz.weatherforcast.screens.about
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.startappz.weatherforcast.R
+import com.startappz.weatherforcast.widgets.WeatherAppBar
 
 @Composable
 fun AboutScreen(navController: NavHostController) {
-    Text(text = "About Screen")
-    
+    Scaffold(
+        modifier = Modifier.fillMaxWidth(),
+        topBar = {
+            WeatherAppBar(
+                title = "About",
+                navController = navController,
+                icon = Icons.AutoMirrored.Default.ArrowBack,
+                isMainScreen = false,
+                onButtonClick = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    ) {
+        it.toString()
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = stringResource(id = R.string.about_app),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Bold)
+
+                Text(text = stringResource(id = R.string.api_used),
+                    style = MaterialTheme.typography.bodySmall,
+                    fontWeight = FontWeight.Light)
+            }
+        }
+    }
+
 }
